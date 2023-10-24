1fa4eec4e2f0542dfe30815484709918f34072e9
Jack Psaras <jack.psaras@uconn.edu>
2023-10-24 16:54:35 -0400

    connected the api and database to course create page

--- src/views/AdminCreateCourseView.vue
+++ src/views/AdminCreateCourseView.vue
@@ -1,65 +1,116 @@
 <template>
   <main class="admincreatecourse">
     <h2>Create Course</h2>
-    <p>
-      Admin - Create Course Page
-    </p>
+    <p>Admin - Create Course Page</p>
 
-    <form class="create-course">
-      <label for="todo">New Course</label>
-      <label for="department">Department</label>
-      <input type="text" id="department" name="Department" v-model="department" pattern="[A-Z]{4}" required />
-      <p> Required: Should consist of 4 letters A-Z </p>
-      <label for="coursenumber">Course Number</label>
-      <input type="text" id="coursenumber" name="CourseNumber" v-model="coursenumber" pattern="\d{4}" required />
-      <p> Required: Should consist of 4 numbers 0-9 </p>
-      <label for="coursename">Name</label>
-      <input type="text" id="coursename" name="Name" v-model="coursename" maxlength="250" required />
-      <p> Required: Should not be longer than 250 characters in length </p>
-      <label for="professor">Professor(s)</label>
-      <input type="text" id="professor" name="Professor" v-model="professor" required />
-      <p> Required </p>
-      <label for="starttime">Meeting Start Time</label>
-      <input type="time" id="starttime" name="Meeting Start Time" v-model="starttime" required/>
-      <p> Required </p>
-      <label for="endtime">Meeting End Time</label>
-      <input type="time" id="endtime" name="Meeting End Time" v-model="endtime" required />
-      <p> Required </p>
-      <label for="location">Meeting Location</label>
-      <input type="text" id="location" name="Meeting Location" v-model="location" required />
-      <p> Required </p>
-      <label for="credits">Credits</label>
-      <input type="number" id="credits" name="Credits" v-model="credits" min="1.00" max="15.00" step="1.00" required/>
-      <p> Required: Can be any whole credit amount from 1.00 - 15.00 </p>
-      <label for="capacity">Capacity</label>
-      <input type="text" id="capacity" name="Capacity" v-model="capacity" required/>
-      <p> Required </p>
-      <div class="waitlist">
-        <label for="waitlist">Waitlist Open</label>
-        <input type="checkbox" id="waitlist" name="WaitlistOpen" v-model="waitlist" required/>
-        <p> Required </p>
+    <form class="create-course" @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="name">Course Name (e.g., Math 101)</label>
+        <input type="text" id="name" name="Name" v-model="name" required />
+        <p class="requirement-note">Required</p>
       </div>
-      <label for="coursetype">Course Type</label>
-      <select id="coursetype" name="Course Type" v-model="coursetype" required>
-        <option value="lecture">Lecture</option>
-        <option value="discussion">Discussion</option>
-        <option value="lab">Lab</option>
-        <option value="seminar">Seminar</option>
-      </select>
-      <p> Required </p>
-      <label for="prereqs">Pre-requirements</label>
-      <input type="text" id="prereqs" name="Pre-requirements" v-model="prereqs" />
-      <p> Optional </p>
-      <label for="description">Description</label>
-      <textarea id="description" name="description" v-model="description" ></textarea>
-      <p> Optional </p>
-      <button type="reset">Clear</button>
-      <button type="submit">Save</button>
+
+      <div class="form-group">
+        <label for="days">Days (e.g., Mon, Wed, Fri)</label>
+        <input type="text" id="days" name="Days" v-model="days" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="location">Location (e.g., Room 101)</label>
+        <input type="text" id="location" name="Location" v-model="location" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="major">Major (e.g., Computer Science)</label>
+        <input type="text" id="major" name="Major" v-model="major" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="professor">Professor(s) (e.g., Dr. Smith)</label>
+        <input type="text" id="professor" name="Professor" v-model="professor" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="seats">Seats (e.g., 30)</label>
+        <input type="number" id="seats" name="Seats" v-model="seats" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="time">Time (e.g., 10:00 AM - 11:30 AM)</label>
+        <input type="text" id="time" name ="Time" v-model="time" required />
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <div class="form-group">
+        <label for="type">Course Type</label>
+        <select id="type" name="Type" v-model="type" required>
+          <option value="lecture">Lecture</option>
+          <option value="discussion">Discussion</option>
+          <option value="lab">Lab</option>
+          <option value="seminar">Seminar</option>
+        </select>
+        <p class="requirement-note">Required</p>
+      </div>
+
+      <!-- Your other form inputs as needed -->
+
+      <div class="form-group">
+        <button type="reset" class="btn-clear">Clear</button>
+        <button type="submit" class="btn-save">Save</button>
+      </div>
     </form>
-  </main>  
+  </main>
 </template>
 
-<style>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: '',
+      days: '',
+      location: '',
+      major: '',
+      professor: '',
+      seats: '',
+      time: '',
+      type: 'lecture',
+      // Other data properties as needed
+    };
+  },
+  methods: {
+    submitForm() {
+      const courseData = {
+        name: this.name,
+        days: this.days,
+        location: this.location,
+        major: this.major,
+        professor: this.professor,
+        seats: this.seats,
+        time: this.time,
+        type: this.type,
+        // Other data properties as needed
+      };
+
+      axios.post('https://8dbuywnj95.execute-api.us-east-1.amazonaws.com/Final_stage_course/course', courseData)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
 .create-course {
   max-width: 400px;
   margin: 0 auto;
@@ -69,64 +120,49 @@
   border-radius: 5px;
 }
 
-.create-course select {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-  background-color: white;
-  color: #333;
+.form-group {
+  margin-bottom: 20px;
 }
 
-.create-course textarea {
-  width: 100%;
-  height: 150px; /* Adjust the height as needed */
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-.create-course label {
+label {
   display: block;
-  margin-bottom: 5px;
   font-weight: bold;
 }
 
-.create-course p {
-  margin-bottom: 10px;
-}
-
-.create-course input.invalid {
-  border: 5px solid red;
-}
-
-.create-course input {
+input[type="text"],
+select {
   width: 100%;
   padding: 10px;
-  margin-bottom: 5px;
   border: 1px solid #ccc;
   border-radius: 3px;
 }
 
-.create-course button {
-  display: inline-block;
+input[type="number"] {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.btn-clear {
+  background-color: #ccc;
+  color: #333;
+  border: none;
+  border-radius: 5px;
   padding: 10px 20px;
+  margin-right: 10px; /* Add margin between buttons */
+}
+
+.btn-save {
   background-color: #007BFF;
   color: #fff;
   border: none;
-  border-radius: 3px;
-  cursor: pointer;
-  margin-right: 10px;
+  border-radius: 5px;
+  padding: 10px 20px;
 }
 
-.create-course button[type="reset"] {
-  background-color: #ccc;
+.requirement-note {
+  font-size: 12px;
+  color: #999;
 }
-
-.create-course button[type="submit"] {
-  background-color: #28a745;
-}
-
 </style>
