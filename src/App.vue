93a8e4b8da6b0e8ba85334cae20c48d7a0527b61
Novrus Shehaj <Novrus.Shehaj@uconn.edu>
2023-11-01 16:27:11 -0400

    Course View Tab
    Course View Tab added and template design for page implemented.

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,25 @@
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
   data() {
     return {
       tabs: [
-        { path: '/', label: 'Home' },
-        { path: '/fetch', label: 'Create an Account' },
-        { path: { name: 'form' }, label: 'Form Example' },
-        { path: '/schedule', label: 'Schedule View' },
-        { path: '/admin/createcourse', label: 'Create Course' },
-        { path: '/login', label: 'Login' },
-      ]
+        { path: "/", label: "Home" },
+        { path: "/fetch", label: "Create an Account" },
+        { path: { name: "form" }, label: "Form Example" },
+        { path: "/schedule", label: "Schedule View" },
+        { path: "/mycourses", label: "Course View" },
+        { path: "/admin/createcourse", label: "Create Course" },
+        { path: "/login", label: "Login" },
+      ],
     };
   },
   components: {
     Header,
-    Footer
-  }
+    Footer,
+  },
 };
 </script>
